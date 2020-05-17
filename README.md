5513f020f36c4a173a752bd602cd27eb53b99744
manemone <hamada.kazuki.mum@gmail.com>
2020-05-18 03:02:11 +0900

    Add test TS code

---
+++ README.md
@@ -0,0 +1,22 @@
+# lifegame-ts
+## What's this?
+An implementation for learning TypeScript.
+
+## Dependencies
+- npm
+- webpack
+
+## Setting up
+```bash
+$ npm install
+```
+
+## Build
+```bash
+$ npm run build
+```
+
+## Run
+```bash
+$ open index.html
+```
---
+++ ts/index.ts
@@ -0,0 +1,19 @@
+class User {
+  public age: number;
+  public familyName: string;
+  public givenName: string;
+
+  constructor (familyName: string, givenName: string, age: number) {
+    this.age = age;
+    this.familyName = familyName;
+    this.givenName = givenName;
+  }
+}
+
+const user = new User('Doe', 'John', 20);
+
+const contentsElem = document.getElementById('contents');
+
+if (!!contentsElem) {
+  contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+}
