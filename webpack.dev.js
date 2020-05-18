650f7429a8071756937f6df46e850adf1c104ac8
manemone <hamada.kazuki.mum@gmail.com>
2020-05-19 03:53:35 +0900

    Introduce separate tsconfig file for each dev environment

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,21 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
-module.exports = merge(common, {
+module.exports = merge.smart(common, {
   // Source map will be generated for debugging on browsers
-    mode: 'development',
+  mode: 'development',
+
+  module: {
+    rules: [{
+      // Files has `ts` suffix
+      test: /\.ts$/,
+      // will be compiled as TypeScript code
+      use: [{
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.dev.json'
+        }
+      }]
+    }]
+  },
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,20 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
-module.exports = merge(common, {
-    mode: 'production',
+module.exports = merge.smart(common, {
+  mode: 'production',
+
+  module: {
+    rules: [{
+      // Files has `ts` suffix
+      test: /\.ts$/,
+      // will be compiled as TypeScript code
+      use: [{
+        loader: 'ts-loader',
+        options: {
+          configFile: 'tsconfig.prod.json'
+        }
+      }]
+    }]
+  },
 })
