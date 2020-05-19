f91938efbfd7a48a862dfee223d85864b5e22b47
manemone <hamada.kazuki.mum@gmail.com>
2020-05-20 04:06:06 +0900

    Introduce image file handling on build

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -6,4 +6,9 @@ const contentsElem = document.getElementById('contents');
 
 if (!!contentsElem) {
   contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+
+  const imgElem = document.createElement('img');
+  const img = require('../assets/images/campbel_can.jpg');
+  imgElem.src = img.default;
+  contentsElem.appendChild(imgElem);
 }
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
   // The main js file. The whole script will be executed from this file
@@ -15,16 +16,47 @@ module.exports = {
     new HtmlWebpackPlugin({
       file: path.join(__dirname, 'dist', 'index.html'),
       template: 'src/index.html'
-    })
+    }),
+
+    // Copy all files under `src/assets` into `dist/assets` directory
+
+    // Optimize all images in project directory on build
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      pngquant: {
+        verbose: true,
+        quality: '80-90',
+      }
+    }),
   ],
 
   module: {
-    rules: [{
-      // Files has `ts` suffix
-      test: /\.ts$/,
-      // will be compiled as TypeScript code
-      use: 'ts-loader'
-    }]
+    rules: [
+      {
+        // Files has `ts` suffix
+        test: /\.ts$/,
+        // will be compiled as TypeScript code
+        use: 'ts-loader'
+      },
+      {
+        // Copy asset files into dist directory
+        test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
+        loader: 'file-loader',
+        options: {
+          context: 'src/assets',
+
+          // Filename will not be changed unless the content of the asset is changed
+          name: '[name]-[contenthash].[ext]',
+
+          // Keep the original directory structure under the output path
+          outputPath: (name, resourcePath, context) => {
+            const relativePath = path.relative(context, resourcePath);
+            const dir = path.dirname(relativePath);
+            return `assets/${dir}/${name}`;
+          },
+        }
+      },
+    ]
   },
   // To resolve `.ts` files on import statement
   resolve: {
