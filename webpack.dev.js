921eb26885925ab6d51a295c0033ce1c1ff0a491
manemone <hamada.kazuki.mum@gmail.com>
2020-05-20 03:00:45 +0900

    Introduce dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 
 module.exports = merge.smart(common, {
   // Source map will be generated for debugging on browsers
   mode: 'development',
+
+  devtool: 'inline-source-map',
 
   module: {
     rules: [{
@@ -18,4 +21,13 @@ module.exports = merge.smart(common, {
       }]
     }]
   },
+
+  devServer: {
+    // Set directory for source code
+    contentBase: path.join(__dirname, 'src'),
+    port: 8080,
+
+    host: '0.0.0.0',
+    hot: true
+  }
 })
